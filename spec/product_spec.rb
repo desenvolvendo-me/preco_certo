2cc1b8c011bc9f49a790c112d5d324d3ef16051c
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-05-20 21:52:02 -0300

    refactor(preco_certo): correcting rubocop offenses

---
+++ spec/product_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+require "preco_certo/product"
+
+RSpec.describe "Product" do
+  let(:products) { Product.products }
+
+  it "create product" do
+    product = Product.create(6, "Ford EcoSport", "UN")
+    expect(product.id_product).to eq(6)
+    expect(product.description).to eq("Ford EcoSport")
+    expect(product.unity).to eq("UN")
+  end
+
+  it "get all products" do
+    expect(products.length).to eq(5)
+  end
+
+  it "get first product" do
+    expect(products.first["id_product"]).to eq("1")
+    expect(products.first["description"]).to eq("Chevrolet Onix")
+    expect(products.first["unity"]).to eq("UN")
+  end
+end
+
+
