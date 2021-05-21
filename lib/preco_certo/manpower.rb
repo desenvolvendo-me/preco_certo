9e794c5f3e7a6358f641aca6c2fdb28cf57dddfc
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-05-21 08:31:04 -0300

    feat(preco_certo): added ManPower Class

---
+++ lib/preco_certo/manpower.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "preco_certo/data_parse"
+
+# class Product, CSV product
+class ManPower
+  attr_accessor :id_manpower, :description, :time
+
+  def initialize(id_manpower, description, time)
+    @id_manpower = id_manpower
+    @description = description
+    @time = time
+  end
+
+  def self.manpowers
+    data_parse = DataParse.new("preco_certo/storage/manpower.csv").parse!
+    data_parse.each do |line|
+      ManPower.new(
+        line["id_manpower"],
+        line["description"],
+        line["time"]
+      )
+    end
+  end
+
+  def self.create(id_manpower, description, time)
+    ManPower.new(id_manpower, description, time)
+  end
+end
