17e52481a87d4bf4282ba320b6675ddbe39f4c31
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2021-05-27 09:15:17 -0300

    refactor(lib/preco_certo): fix require order

--- lib/preco_certo.rb
+++ lib/preco_certo.rb
@@ -8,8 +8,8 @@ require_relative "preco_certo/employee"
 require_relative "preco_certo/manpower"
 require_relative "preco_certo/product"
 require_relative "preco_certo/products_manpower"
-require_relative "preco_certo/raw_material"
 require_relative "preco_certo/product_raw_material"
+require_relative "preco_certo/raw_material"
 
 # Preco Certo module Gem
 module PrecoCerto
