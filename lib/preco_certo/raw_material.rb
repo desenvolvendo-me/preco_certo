9290f891c7d36c4cd8429558860392f65d22dce7
Lukas Pol <lukaspaespol@gmail.com>
2021-05-27 07:16:44 -0300

    chore: remove heritage, refactor methods using Data Parse methods and refactor test

--- lib/preco_certo/raw_material.rb
+++ lib/preco_certo/raw_material.rb
@@ -3,14 +3,36 @@
 require "preco_certo/data_parse"
 
 # raw_material.rb
-class RawMaterial < DataParse
-  def raw_materials
-    parse!
+class RawMaterial
+  def initialize(id, name, price, icms, ipi, pis, cofins)
+    @id = id
+    @name = name
+    @price = price
+    @icms = icms
+    @ipi = ipi
+    @pis = pis
+    @cofins = cofins
   end
 
-  def create(attributes)
-    CSV.open(file, "a", options) do |csv|
-      csv << attributes
+  def self.raw_materials
+    file = "preco_certo/storage/raw_materials.csv"
+    data_parse = DataParse.new(file).parse!
+    data_parse.each do |line|
+      RawMaterial.new(
+        line["id"],
+        line["name"],
+        line["price"],
+        line["icms"],
+        line["ipi"],
+        line["pis"],
+        line["cofins"],
+      )
     end
+  end
+
+  def self.create(attributes)
+    file = "preco_certo/storage/raw_materials.csv"
+
+    DataParse.new(file).create(attributes)
   end
 end
