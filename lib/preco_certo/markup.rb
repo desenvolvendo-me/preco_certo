21291c099a2328fc72015771f52958fa84856846
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-05-27 21:40:34 -0300

    feat(preco_certo): added method calculate index of the markup

--- lib/preco_certo/markup.rb
+++ lib/preco_certo/markup.rb
@@ -37,6 +37,16 @@ class Markup
     end
   end
 
+  def self.calculate_index(id_markup)
+    data_parse = DataParse.new("preco_certo/storage/markup.csv").parse!
+    indice = 0.00
+    # verificar melhor maneira de filtrar o array
+    data_parse.each do |line|
+      indice = line["profit"].to_f + line["commission"].to_f + line["shipping"].to_f + line["marketing"].to_f + line["icms"].to_f + line["ipi"].to_f + line["pis"].to_f + line["cofins"].to_f if line["id_markup"] == id_markup
+    end
+    (100/(100-indice)).round(8)
+  end
+
   def self.create(id_markup, description, profit, commission, shipping, marketing, icms, ipi, pis, cofins)
     Markup.new(id_markup, description, profit, commission, shipping, marketing, icms, ipi, pis, cofins)
   end
