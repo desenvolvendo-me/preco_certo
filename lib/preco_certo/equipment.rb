da84f85886a4d82658feb33612e7640eae43a8ce
williamtome <9632783335245523>
2021-06-02 09:21:30 -0300

    style: change for short name variable.

--- lib/preco_certo/equipment.rb
+++ lib/preco_certo/equipment.rb
@@ -6,22 +6,22 @@ class Equipment
   ID_RANDOM_SET = 2000
   CSV_PATH = "lib/preco_certo/storage/equipments.csv"
 
-  attr_reader :id, :description, :aquisition_date, :value, :annual_percentage
+  attr_reader :id, :description, :aquisition_date, :value, :annual_percent
 
-  def initialize(id, description, aquisition_date, value, annual_percentage)
+  def initialize(id, description, aquisition_date, value, annual_percent)
     @id = id
     @description = description
     @aquisition_date = aquisition_date
     @value = value
-    @annual_percentage = annual_percentage
+    @annual_percent = annual_percent
   end
 
-  def self.create(description, aquisition_date, value, annual_percentage)
+  def self.create(description, aquisition_date, value, annual_percent)
     id = rand(ID_RANDOM_SET)
-    equipment = Equipment.new(id, description, aquisition_date, value, annual_percentage)
+    equipment = Equipment.new(id, description, aquisition_date, value, annual_percent)
 
     CSV.open(CSV_PATH, "ab") do |csv|
-      csv << [equipment.id, equipment.description, equipment.aquisition_date, equipment.value, equipment.annual_percentage]
+      csv << [equipment.id, equipment.description, equipment.aquisition_date, equipment.value, equipment.annual_percent]
     end
 
     equipment
