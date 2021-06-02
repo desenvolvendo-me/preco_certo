1443ef1075330e483d937eb638b21e4cc53bc21d
Arlisson Jaques <arlissonjaques@gmail.com>
2021-06-02 14:31:24 -0300

    test(company_spec): added tests for cvs headers

--- spec/company_spec.rb
+++ spec/company_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Company" do
   let!(:company) { Company.new("preco_certo/storage/companies.csv") }
 
   before do
-    company.create(6,"empresa5", "real_profit", "8655559696", "52744494000112")
+    company.create(6,"empresa5", "real_profit", "8655559696", "52744494000112", 1.2, 3, 4, 2)
   end
 
   let(:created_company) { company.companies[company.companies.length - 1] }
@@ -15,6 +15,17 @@ RSpec.describe "Company" do
       expect(created_company["tax_regime"]).to eq("real_profit")
       expect(created_company["phone"]).to eq("8655559696")
       expect(created_company["cnpj"]).to eq("52744494000112")
+      expect(created_company["icms"]).to eq("1.2")
+      expect(created_company["ipi"]).to eq("3")
+      expect(created_company["pis"]).to eq("4")
+      expect(created_company["cofins"]).to eq("2")
     end
+
+    it "check csv headers" do
+      array_headers = %w[id name tax_regime phone cnpj icms ipi pis cofins]
+      expect(created_company.headers).to eq(array_headers)
+      expect(created_company.headers.length).to eq(9)
+    end
+
   end
 end
