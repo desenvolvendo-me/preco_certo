dd5b0e969c6c3d57e36580ef548c7d32237b8aec
Felipe Augusto Rosa <flp.far@gmail.com>
2021-06-02 21:59:40 -0300

    test: new columns in product manpower create

--- spec/products_manpower_spec.rb
+++ spec/products_manpower_spec.rb
@@ -4,13 +4,16 @@ RSpec.describe "Product_ManPower" do
   let(:productsmanpowers) { ProductManPower.productsmanpower }
 
   it "create ProductManPower" do
-    product_manpower = ProductManPower.create(6, "Montagem dos Bancos", "Luciano Paulista", "1200.00", 8.50, 15.50)
+    product_manpower = ProductManPower.create(6, "Montagem dos Bancos", "Luciano Paulista",
+                                              "1200.00", 8.50, 15.50, 0, 10)
     expect(product_manpower.id_product).to eq(6)
     expect(product_manpower.operation).to eq("Montagem dos Bancos")
     expect(product_manpower.employee).to eq("Luciano Paulista")
     expect(product_manpower.salary).to eq("1200.00")
     expect(product_manpower.time).to eq(8.50)
     expect(product_manpower.cost_mo).to eq(15.50)
+    expect(product_manpower.type_operation).to eq(0)
+    expect(product_manpower.id_equipment).to eq(10)
   end
 
   it "get all products_manpower" do
