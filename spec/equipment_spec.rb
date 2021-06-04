c705fce10358a9c80c3f61c26a996474a94efc2d
williamtome <william.tomenh@gmail.com>
2021-06-03 21:05:42 -0300

    test: calculate actual value of the equipment.

--- spec/equipment_spec.rb
+++ spec/equipment_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "date"
+
 RSpec.describe "Equipment" do
   csv_path = "lib/preco_certo/storage/equipments.csv"
 
@@ -17,6 +19,21 @@ RSpec.describe "Equipment" do
     expect(equipment.aquisition_date).to eq("01/01/2021")
     expect(equipment.value).to eq(180_000_00)
     expect(equipment.annual_percent).to eq(10)
+  end
+
+  it "calculate actual value of the equipment" do
+    equipment_value = 180000.00
+    annual_percent = 10
+    aquisition_date = Date.parse("01/01/2021")
+    today = Date.parse("31/12/2021")
+    total_days_of_year = 365
+
+    value_per_year = sprintf("%.2f", equipment_value / (100 / annual_percent) / total_days_of_year).to_f
+
+    days_between_today_and_aquisition_date = (today - aquisition_date).to_i
+    actual_value_equipment = equipment_value - (days_between_today_and_aquisition_date * value_per_year)
+
+    expect(162047.52).to eq(actual_value_equipment)
   end
 
   def restart_csv(file_path)
