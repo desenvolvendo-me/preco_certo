d413a9eac80ce0d7d5aaa8c0dea5a214e5a21860
Felipe Augusto Rosa <flp.far@gmail.com>
2021-06-09 07:23:42 -0300

    refactor: company creation and reading

--- lib/preco_certo/company.rb
+++ lib/preco_certo/company.rb
@@ -1,16 +1,29 @@
 # frozen_string_literal: true
 
-require "preco_certo/data_parse"
-
 # company.rb
-class Company < DataParse
-  def companies
-    parse!
+class Company
+  def initialize(id, name, tax_regime, phone, cnpj, icms, ipi, pis, cofins, payroll_percentage)
+    @id = id
+    @name = name
+    @tax_regime = tax_regime
+    @phone = phone
+    @cnpj = cnpj
+    @icms = icms
+    @ipi = ipi
+    @pis = pis
+    @cofins = cofins
+    @payroll_percentage = payroll_percentage
   end
 
-  def create(id, name, tax_regime, phone, cnpj, icms, ipi, pis, cofins, payroll_percentage)
-    CSV.open(file, "a", **options) do |csv|
+  def self.all
+    DataParse.new("preco_certo/storage/companies.csv").parse!
+  end
+
+  def self.create(id, name, tax_regime, phone, cnpj, icms, ipi, pis, cofins, payroll_percentage)
+    CSV.open("lib/preco_certo/storage/companies.csv", "a", col_sep: ";", headers: true) do |csv|
       csv << [id, name, tax_regime, phone, cnpj, icms, ipi, pis, cofins, payroll_percentage]
     end
+
+    Company.new(id, name, tax_regime, phone, cnpj, icms, ipi, pis, cofins, payroll_percentage)
   end
 end
