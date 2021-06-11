596bb1b1ca28a12b4e30f8e1fcad2d8b21bee24d
Felipe Augusto Rosa <flp.far@gmail.com>
2021-06-11 09:02:24 -0300

    refactor: company .all return

--- lib/preco_certo/company.rb
+++ lib/preco_certo/company.rb
@@ -2,6 +2,8 @@
 
 # company.rb
 class Company
+  attr_reader :id, :name, :tax_regime, :phone, :cnpj, :icms, :ipi, :pis, :cofins, :payroll_percentage
+
   def initialize(id, name, tax_regime, phone, cnpj, icms, ipi, pis, cofins, payroll_percentage)
     @id = id
     @name = name
@@ -16,7 +18,22 @@ class Company
   end
 
   def self.all
-    DataParse.new("preco_certo/storage/companies.csv").parse!
+    data_parse = DataParse.new("preco_certo/storage/companies.csv").parse!
+
+    data_parse.map do |company|
+      Company.new(
+        company["id"],
+        company["name"],
+        company["tax_regime"],
+        company["phone"],
+        company["cnpj"],
+        company["icms"],
+        company["ipi"],
+        company["pis"],
+        company["cofins"],
+        company["payroll_percentage"]
+      )
+    end
   end
 
   def self.create(id, name, tax_regime, phone, cnpj, icms, ipi, pis, cofins, payroll_percentage)
--- spec/company_spec.rb
+++ spec/company_spec.rb
@@ -9,21 +9,22 @@ RSpec.describe "Company" do
 
   context "validations csv" do
     it "create a company" do
-      expect(created_company["name"]).to eq("empresa5")
-      expect(created_company["tax_regime"]).to eq("real_profit")
-      expect(created_company["phone"]).to eq("8655559696")
-      expect(created_company["cnpj"]).to eq("52744494000112")
-      expect(created_company["icms"]).to eq("1.2")
-      expect(created_company["ipi"]).to eq("3")
-      expect(created_company["pis"]).to eq("4")
-      expect(created_company["cofins"]).to eq("2")
-      expect(created_company["payroll_percentage"]).to eq("20")
+      expect(created_company.name).to eq("empresa5")
+      expect(created_company.tax_regime).to eq("real_profit")
+      expect(created_company.phone).to eq("8655559696")
+      expect(created_company.cnpj).to eq("52744494000112")
+      expect(created_company.icms).to eq("1.2")
+      expect(created_company.ipi).to eq("3")
+      expect(created_company.pis).to eq("4")
+      expect(created_company.cofins).to eq("2")
+      expect(created_company.payroll_percentage).to eq("20")
     end
 
     it "check csv headers" do
-      array_headers = %w[id name tax_regime phone cnpj icms ipi pis cofins payroll_percentage]
-      expect(created_company.headers).to eq(array_headers)
-      expect(created_company.headers.length).to eq(10)
+      headers = CSV.open("lib/preco_certo/storage/companies.csv", col_sep: ";", &:readline)
+
+      expect(headers.length).to eq(10)
+      expect(headers).to eq(%w[id name tax_regime phone cnpj icms ipi pis cofins payroll_percentage])
     end
   end
 end
