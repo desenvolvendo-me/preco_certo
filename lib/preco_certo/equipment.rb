ed95272f83a6d6997455cd5e7ee71858bc78a1bb
williamtome <william.tomenh@gmail.com>
2021-06-10 21:57:14 -0300

    fix: return result converted to float.

--- lib/preco_certo/equipment.rb
+++ lib/preco_certo/equipment.rb
@@ -53,7 +53,8 @@ class Equipment
   def deprecation_month
     percent = format("%.2f", 100 / annual_percent.to_f).to_f
     per_month = (percent * 12).to_f
-    format("%.2f", value.to_f / per_month.to_f)
+    result = format("%.2f", value.to_f / per_month.to_f)
+    result.to_f
   end
 
   def self.total_deprecation
