ca9e93339183e8250ab3edffbe2b49be47296458
Lukas Pol <lukaspaespol@gmail.com>
2021-06-16 07:30:27 -0300

    feat(company): create controller

---
+++ spec/requests/api/v1/companies_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+
+RSpec.describe "/api/v1/companies", type: :request do
+  let(:valid_attributes) { build(:company).attributes }
+
+  let(:invalid_attributes) {
+    {
+      name: "",
+      cpnj: "",
+      tax_regime: "",
+    }
+  }
+
+  describe "GET /index" do
+    it "renders a successful response" do
+      Company.create! valid_attributes
+      get api_v1_companies_url, as: :json
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /show" do
+    it "renders a successful response" do
+      company = Company.create! valid_attributes
+      get api_v1_company_url(company), as: :json
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a new Company" do
+        expect {
+          post api_v1_companies_url,
+               params: { company: valid_attributes }, as: :json
+        }.to change(Company, :count).by(1)
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new Api::V1::Company" do
+        expect {
+          post api_v1_companies_url,
+               params: { company: invalid_attributes }, as: :json
+        }.to change(Company, :count).by(0)
+      end
+
+      it "renders a JSON response with errors for the new api/v1_company" do
+        post api_v1_companies_url,
+             params: { company: invalid_attributes }, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+        skip("Add a hash of attributes valid for your model")
+      }
+
+      it "updates the requested api/v1_company" do
+        company = Company.create! valid_attributes
+        patch api_v1_company_url(company),
+              params: { company: new_attributes }, as: :json
+        company.reload
+        skip("Add assertions for updated state")
+      end
+    end
+
+    context "with invalid parameters" do
+      it "renders a JSON response with errors for the api/v1_company" do
+        company = Company.create! valid_attributes
+        patch api_v1_company_url(company),
+              params: { company: invalid_attributes }, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "destroys the requested api/v1_company" do
+      company = Company.create! valid_attributes
+      expect {
+        delete api_v1_company_url(company), as: :json
+      }.to change(Company, :count).by(-1)
+    end
+  end
+end
