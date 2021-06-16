d10f233075584dec919089af1324c8ff7919d23e
Lukas Pol <lukaspaespol@gmail.com>
2021-06-16 07:29:53 -0300

    create seeds for company

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Company.create!(
+  name: "Ford",
+  phone: "123112231",
+  cnpj: "12391919123123",
+  tax_regime: 1,
+  icms: 2.0,
+  ipi: 5,
+  pis: 1,
+  cofins: 3.5,
+  payroll_percentage: 5.9,
+)
