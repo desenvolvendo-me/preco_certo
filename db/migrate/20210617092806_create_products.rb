0aebbccfc3d2c8a15d54346d0fbae57ab88ca17f
Lukas Pol <lukaspaespol@gmail.com>
2021-06-17 07:24:18 -0300

    feat(product): create model and factory bot

---
+++ db/migrate/20210617092806_create_products.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :description
+      t.string :unity
+
+      t.timestamps
+    end
+  end
+end
