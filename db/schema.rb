fcf2f5e253b579939c4f454e2574d74ca3cb6f1c
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-06-18 17:15:14 -0300

    feat(raw_material): git schema db for raw_material

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_092826) do
+ActiveRecord::Schema.define(version: 2021_06_17_092331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2021_06_16_092826) do
     t.float "pis"
     t.float "cofins"
     t.float "payroll_percentage"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "raw_materials", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.float "icms"
+    t.float "ipi"
+    t.float "pis"
+    t.float "cofins"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
