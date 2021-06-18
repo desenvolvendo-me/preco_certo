27365670ed7e4494868564eb6d464fbf48040238
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2021-06-18 06:12:39 -0300

    refactor(schema): added create manpowers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_092826) do
+ActiveRecord::Schema.define(version: 2021_06_17_091704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_06_16_092826) do
     t.float "pis"
     t.float "cofins"
     t.float "payroll_percentage"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "manpowers", force: :cascade do |t|
+    t.string "description"
+    t.float "time_hex"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
