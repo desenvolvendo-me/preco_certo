45e5f1422851290a9f6c1c986788611bf279c18f
Lukas Pol <lukaspaespol@gmail.com>
2021-06-18 07:07:24 -0300

    feat(company): add `has_many` products and expenses

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
+  has_many :expenses
+  has_many :products
+
   validates :name, :cnpj, :tax_regime, presence: true
 
   enum tax_regime: ["simples nacional", "lucro presumido", "lucro real"]
