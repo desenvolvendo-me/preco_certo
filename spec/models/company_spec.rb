b0f34cb949868835070fd503e7b530612ab6576d
Lukas Pol <lukaspaespol@gmail.com>
2021-06-18 07:08:34 -0300

    test(company model): create test to validations

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,7 +3,15 @@
 require "rails_helper"
 
 RSpec.describe Company, type: :model do
-  it { is_expected.to validate_presence_of :name }
-  it { is_expected.to validate_presence_of :cnpj }
-  it { is_expected.to validate_presence_of :tax_regime }
+  describe "validations" do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :cnpj }
+    it { is_expected.to validate_presence_of :tax_regime }
+  end
+
+  describe "associations" do
+    it { should have_many(:expenses) }
+    it { should have_many(:products) }
+  end
+
 end
