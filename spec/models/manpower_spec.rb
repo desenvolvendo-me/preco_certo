4a891f13d50e4a487d9273c36f9d28d5a2cf9a68
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2021-06-18 06:20:43 -0300

    refactor(spec/models): addde manpower_spec.rb

---
+++ spec/models/manpower_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Manpower, type: :model do
+  it { is_expected.to validate_presence_of :description }
+  it { is_expected.to validate_presence_of :time_hex }
+end
