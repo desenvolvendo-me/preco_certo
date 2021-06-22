c4ce8d47d5e1b0e1bfd789f03feb981f7bc4d411
Lukas Pol <lukaspaespol@gmail.com>
2021-06-22 06:59:06 -0300

    test(product_raw_material): test calculate raw material cost

--- spec/models/product_raw_material_spec.rb
+++ spec/models/product_raw_material_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe ProductRawMaterial, type: :model do
     it { should belong_to(:product) }
     it { should belong_to(:raw_material) }
   end
+
+  describe "calculate" do
+    it "raw material cost" do
+      raw_material = create(:raw_material, net_price: 2)
+      product_raw_material = create(:product_raw_material, consumption: 5, raw_material: raw_material)
+
+      expect(product_raw_material.raw_material_cost).to eq(10.0)
+    end
+  end
 end
