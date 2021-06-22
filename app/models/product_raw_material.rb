8819de2bd01d7d719d324cc225e2b49c501271a4
Lukas Pol <lukaspaespol@gmail.com>
2021-06-22 06:58:52 -0300

    feat(product_raw_material): calculate raw material cost after create

--- app/models/product_raw_material.rb
+++ app/models/product_raw_material.rb
@@ -3,4 +3,14 @@
 class ProductRawMaterial < ApplicationRecord
   belongs_to :product
   belongs_to :raw_material
+
+  after_create :calculate_raw_material_cost
+
+  private
+
+  def calculate_raw_material_cost
+    cost = raw_material.net_price * consumption
+
+    update(raw_material_cost: cost)
+  end
 end
