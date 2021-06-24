ea127cec41ada8c1b37023f6c6341e7cc24a0ab5
Lukas Pol <lukaspaespol@gmail.com>
2021-06-24 07:23:14 -0300

    refactor(models): add schema information in models

--- app/models/product_raw_material.rb
+++ app/models/product_raw_material.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: product_raw_materials
+#
+#  id                :bigint(8)        not null, primary key
+#  consumption       :float
+#  raw_material_cost :float
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  product_id        :bigint(8)        not null
+#  raw_material_id   :bigint(8)        not null
+#
+# Indexes
+#
+#  index_product_raw_materials_on_product_id       (product_id)
+#  index_product_raw_materials_on_raw_material_id  (raw_material_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (product_id => products.id)
+#  fk_rails_...  (raw_material_id => raw_materials.id)
+#
+
+
 class ProductRawMaterial < ApplicationRecord
   belongs_to :product
   belongs_to :raw_material
--- app/models/raw_material.rb
+++ app/models/raw_material.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: raw_materials
+#
+#  id         :bigint(8)        not null, primary key
+#  cofins     :float
+#  icms       :float
+#  ipi        :float
+#  name       :string
+#  net_price  :float
+#  pis        :float
+#  price      :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
 class RawMaterial < ApplicationRecord
   has_many :product_raw_materials
 
